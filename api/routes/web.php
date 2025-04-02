85b3569edd584a72c822d7f9546dda33560bbb36
Viktor Karpinski <developer@viktorkarpinski.com>
2025-04-02 17:25:56 +0200

    Meal routes

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FoodCategoryController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\MealController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,4 +19,10 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResource('food', FoodController::class);
     Route::apiResource('food-categories', FoodCategoryController::class);
+
+    Route::get('meals', [MealController::class, 'index']);
+    Route::post('meals/start', [MealController::class, 'start']);
+    Route::post('meals/{meal}/foods', [MealController::class, 'addFood']);
+    Route::post('meals/{meal}/finish', [MealController::class, 'finish']);
+    Route::get('meals/{meal}', [MealController::class, 'show']);
 });
