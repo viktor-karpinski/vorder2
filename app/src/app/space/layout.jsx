537fa3b24118cd7d874bd7764ab2f5f8f6f03274
Viktor Karpinski <developer@viktorkarpinski.com>
2025-05-26 20:30:22 +0200

    Tab Switch

--- app/src/app/space/layout.jsx
+++ app/src/app/space/layout.jsx
@@ -331,6 +331,18 @@ const Layout = ({params, children}) => {
             <aside className="side-box">
                 <CalendarNavigation  />
 
+                <div className="tab-switch">
+                    <button className={sideBarTab === 'workspace' && 'active'} onClick={() => {setSideBarTab('workspace')}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path></svg>
+                    </button>
+                    <button className={sideBarTab === 'routines' && 'active'} onClick={() => {setSideBarTab('routines')}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                    </button>
+                    <button className={sideBarTab === 'food' && 'active'} onClick={() => {setSideBarTab('food')}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 8h1a4 4 0 0 1 0 8h-1"></path><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path><line x1="6" y1="1" x2="6" y2="4"></line><line x1="10" y1="1" x2="10" y2="4"></line><line x1="14" y1="1" x2="14" y2="4"></line></svg>
+                    </button>
+                </div>
+
                 {sideBarTab === 'workspace' && 
                     <div className="workspace-navigator">
                         <button id="add-workspace" onClick={() => {createWorkspace()}}>
